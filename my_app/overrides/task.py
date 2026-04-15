075b4170f4e241bf2e9eba49c92e5d5f4d8cc126
LTNgocDiep <ngocdiepyt91@gmail.com>
2026-04-15 10:18:36 +0000

    split status validation

--- my_app/overrides/task.py
+++ my_app/overrides/task.py
@@ -5,11 +5,9 @@ from erpnext.projects.doctype.task.task import Task
 class CustomTask(Task):
     def validate(self):
         super(CustomTask, self).validate()
-        
 
         if self.is_new():
             return
-
 
         old_doc = self.get_doc_before_save()
         if not old_doc:
@@ -18,33 +16,25 @@ class CustomTask(Task):
         old_status = old_doc.status
         new_status = self.status
 
-
         if old_status == new_status:
             return
-
 
         user_roles = frappe.get_roles(frappe.session.user)
         
         is_manager = "GS Project Manager" in user_roles
         is_user = "GS Projects User" in user_roles
 
-
         if is_manager or "Administrator" in user_roles:
             return
 
-
         if is_user:
 
-            if old_status == "Open" and new_status == "Working":
+            allowed_2way_statuses = ["Open", "Working", "QA Pending"]
+            
+            if old_status in allowed_2way_statuses and new_status in allowed_2way_statuses:
                 return
 
-            if old_status == "Working" and new_status == "QA Pending":
-                return
-            
-            if old_status == "Working" and new_status == "Open":
-                return
-            
-            if old_status == "QA Pending" and new_status == "Working":
+            if old_status == "QA Feedback" and new_status == "Working":
                 return
 
             frappe.throw(
